d69d49378b6ac8aadbb1f95f2c296181648cb5d1
Patryk Filip Gryz <pfgryz@gmail.com>
2024-09-20 14:20:58 +0200

    feat: add calculating distance matrix basing on metric

--- src/clustering/distance.rs
+++ src/clustering/distance.rs
@@ -1,6 +1,29 @@
 use crate::clustering::errors::{ClusterResult, ClusteringError, ClusteringErrorKind};
 use crate::clustering::traits::DistanceMetric;
+use crate::io::fasta::record::FastaRecord;
 use crate::io::traits::Record;
+
+// region distance_matrix()
+
+pub fn distance_matrix<Element, Metric>(elements: &Vec<Element>, metric: &Metric) -> Vec<Vec<f64>>
+where
+    Metric: DistanceMetric<Element>,
+{
+    let size = elements.len();
+    let mut matrix = vec![vec![0.0; size]; size];
+
+    for i in 0..size {
+        for j in 0..size {
+            let distance = metric.distance(&elements[i], &elements[j]).unwrap();
+            matrix[i][j] = distance;
+            matrix[j][i] = distance;
+        }
+    }
+
+    matrix
+}
+
+// endregion
 
 // region HammingDistance
 
@@ -26,6 +49,12 @@ impl DistanceMetric<dyn Record> for HammingDistance {
     }
 }
 
+impl DistanceMetric<FastaRecord> for HammingDistance {
+    fn distance(&self, a: &FastaRecord, b: &FastaRecord) -> ClusterResult<f64> {
+        self.distance(a as &dyn Record, b as &dyn Record)
+    }
+}
+
 // endregion
 
 #[cfg(test)]
@@ -35,12 +64,40 @@ mod tests {
     use crate::io::sequence::Sequence;
     use super::*;
 
-    // region HammingDistance
-
     fn record_from_sequence(id: &str, sequence: &str) -> FastaRecord {
         FastaRecord::new(String::from(id), None, Sequence::new(String::from(sequence)))
     }
 
+    // region distance_matrix()
+
+    #[test]
+    fn test_distance_matrix() {
+        let records = vec![
+            record_from_sequence("X1", "ACTG"),
+            record_from_sequence("X2", "ATTG"),
+            record_from_sequence("X3", "CTGA")
+        ];
+        let expected = vec![
+            vec![0f64, 1f64, 4f64],
+            vec![1f64, 0f64, 3f64],
+            vec![4f64, 3f64, 0f64]
+        ];
+
+        let matrix = distance_matrix(&records, &HammingDistance {});
+
+        assert_eq!(matrix.len(), 3);
+        assert_eq!(matrix[0].len(), 3);
+
+        for i in 0..3 {
+            for j in 0..3 {
+                assert_eq!(matrix[i][j], expected[i][j]);
+            }
+        }
+    }
+
+    // endregion
+
+    // region HammingDistance
     #[test]
     fn test_hamming_distance() {
         let a = record_from_sequence("X1", "ACTG");
